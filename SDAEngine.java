2e382e031d1e6cce12aba2d7ad2b0acd692d4341
AkshitaT <akshita.msit@gmail.com>
2016-05-31 14:40:18 -0600

    Update SDAEngine.java

--- SDAEngine.java
+++ SDAEngine.java
@@ -1,7 +1,8 @@
-
+import org.canova.api.conf.Configuration;
 import org.canova.api.records.reader.RecordReader;
 import org.canova.api.split.FileSplit;
 import org.canova.nd4j.nlp.reader.TfidfRecordReader;
+import org.canova.nd4j.nlp.vectorizer.TfidfVectorizer;
 import org.deeplearning4j.datasets.canova.RecordReaderDataSetIterator;
 import org.deeplearning4j.datasets.iterator.DataSetIterator;
 import org.deeplearning4j.eval.Evaluation;
@@ -23,114 +24,136 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 
 public class SDAEngine {
 
-    private static Logger log = LoggerFactory.getLogger (SDAEngine.class);
+    private static Logger log = LoggerFactory.getLogger(SDAEngine.class);
+
 
     public static void main(String[] args) throws Exception {
 
         int outputNum = 2;
-        int inputNum = 500;
-        int iterations = 10;
-        int seed = 123;
-        int batchSize = 100;
 
 
-        log.info ("Loading the train data....");
+        File trainDirectory = new File("/Users/akshitatyagi/Downloads/Corpuses/Train");
+        File testDirectory = new File("/Users/akshitatyagi/Downloads/Corpuses/Train");
+
+        int batchSize = 50;
+        List<String> labels = Arrays.asList(trainDirectory.list());
+        int numLabels = labels.size();
+
+
+        log.info("Loading the train data....");
         Configuration config = new Configuration();
         config.setInt(TfidfVectorizer.MIN_WORD_FREQUENCY, 15);
+        config.setBoolean(RecordReader.APPEND_LABEL, true);
 
-        TfidfRecordReader readerTrain = new TfidfRecordReader();
-        readerTrain.initialize (config, new FileSplit (new File ("Train/")));                           // Labeled path to the training vectors
-        DataSetIterator trainIter = new RecordReaderDataSetIterator (readerTrain, batchSize);
-
-        int inputNum = readerTrain.getNumFeatures();
-        log.info(" Number of Features: " + inputNum);
+        TfidfRecordReader trainReader = new TfidfRecordReader();
+        trainReader.initialize(config, new FileSplit(trainDirectory));                                  // Labeled path to the training vectors
+        DataSetIterator trainIter = new RecordReaderDataSetIterator(trainReader, batchSize, -1, numLabels);
 
 
-        log.info ("Loading the test data...");
-        TfidfRecordReader readerTest = new TfidfRecordReader ();
-        readerTest.initialize (config, new FileSplit (new File ("Test/")));                             // Labeled path to the text vectors
-        readerTest.setTfidfVectorizer(readerTrain.getTfidfVectorizer());                                                                       //reuse vectorizer
-        DataSetIterator testIter = new RecordReaderDataSetIterator (readerTest, batchSize);
+        int inputNum = trainReader.getNumFeatures();
+        log.info("Number of Features: " + inputNum);
 
 
-        log.info ("Build model....");
-        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder ()
-                .seed (seed)
-                .gradientNormalization (GradientNormalization.ClipElementWiseAbsoluteValue)
-                .gradientNormalizationThreshold (1.0)
-                .iterations (iterations)
-                .updater (Updater.NESTEROVS)
-                .momentum (0.5)
-                .momentumAfter (Collections.singletonMap (3, 0.9))
-                .optimizationAlgo (OptimizationAlgorithm.CONJUGATE_GRADIENT)
-                .list ()
-                .layer (0, new AutoEncoder.Builder ()
-                        .nIn (inputNum)
-                        .nOut (250)
-                        .weightInit (WeightInit.XAVIER)
-                        .lossFunction (LossFunction.RMSE_XENT)
-                        .corruptionLevel (0.3)
-                        .build ()
+        log.info("Loading the test data...");
+        TfidfRecordReader testReader = new TfidfRecordReader();
+        testReader.setTfidfVectorizer(trainReader.getTfidfVectorizer());                                //reuse vectorizer
+        testReader.initialize(config, new FileSplit(testDirectory));                                    // Labeled path to the text vectors
+        DataSetIterator testIter = new RecordReaderDataSetIterator(testReader, batchSize, -1, numLabels);
+
+
+        log.info("Build model....");
+
+        int iterations = 1;
+        int seed = 123;
+        WeightInit weightInit = WeightInit.XAVIER;
+        String activation = "relu";
+        Updater updater = Updater.NESTEROVS;
+        double lr = 1e-4;                                                                          
+        double mu = 0.9;                                                                            
+        double l2 = 5e-6;                                                                          
+        boolean regularization = true;
+
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .seed(seed)
+                .iterations(iterations)
+                .activation(activation)
+                .updater(updater)
+                .gradientNormalization(GradientNormalization.RenormalizeL2PerLayer)
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                .gradientNormalizationThreshold(1.0)
+                .learningRate(lr)
+                .momentum(mu)
+                .regularization(regularization)
+                .l2(l2)
+
+                .list()
+                .layer(0, new AutoEncoder.Builder()
+                        .nIn(inputNum)
+                        .nOut(5000)
+                        .weightInit(weightInit)
+                        .lossFunction(LossFunction.RMSE_XENT)
+                        .corruptionLevel(0.3)
+                        .build()
                 )
-                .layer (1, new AutoEncoder.Builder ()
-                        .nIn (250)
-                        .nOut (125)
-                        .weightInit (WeightInit.XAVIER)
-                        .lossFunction (LossFunction.RMSE_XENT)
-                        .corruptionLevel (0.3)
-                        .build ()
+                .layer(1, new AutoEncoder.Builder()
+                        .nIn(5000)
+                        .nOut(2000)
+                        .weightInit(weightInit)
+                        .lossFunction(LossFunction.RMSE_XENT)
+                        .corruptionLevel(0.3)
+                        .build()
                 )
-                .layer (2, new AutoEncoder.Builder ()
-                        .nIn (125)
-                        .nOut (50)
-                        .weightInit (WeightInit.XAVIER)
-                        .lossFunction (LossFunction.RMSE_XENT)
-                        .corruptionLevel (0.3)
-                        .build ()
+                .layer(2, new AutoEncoder.Builder()
+                        .nIn(2000)
+                        .nOut(1000)
+                        .weightInit(weightInit)
+                        .lossFunction(LossFunction.RMSE_XENT)
+                        .corruptionLevel(0.3)
+                        .build()
                 )
-                .layer (3, new OutputLayer.Builder (LossFunction.NEGATIVELOGLIKELIHOOD)
-                        .activation ("softmax")
-                        .nIn (75)
-                        .nOut (outputNum)
-                        .build ()
+                .layer(3, new OutputLayer.Builder(LossFunction.NEGATIVELOGLIKELIHOOD)
+                        .activation("softmax")
+                        .nIn(1000)
+                        .nOut(outputNum)
+                        .build()
                 )
-                .pretrain (true)
-                .backprop (false)
-                .build ();
+                .pretrain(true)
+                .backprop(false)
+                .build();
 
 
-        MultiLayerNetwork model = new MultiLayerNetwork (conf);
-        model.init ();
-        model.setListeners (Collections.singletonList ((IterationListener) new ScoreIterationListener (1)));
-        //model.setListeners(new HistogramIterationListener(1));        // Generating the Visualizing model
+        MultiLayerNetwork model = new MultiLayerNetwork(conf);
+        model.init();
+        model.setListeners(Collections.singletonList((IterationListener) new ScoreIterationListener(1)));
+        //model.setListeners(new HistogramIterationListener(1));                                                // Generating the Visualizing model
 
 
-        log.info ("Train model....");
-        try {
-            model.fit (trainIter);
-        } catch (Exception ex) {
-            ex.printStackTrace();
+        log.info("Train model....");
+        int epochs = 5;
+        for (int i = 0; i < epochs; i++) {
+            trainIter.reset();
+            model.fit(trainIter);
+            log.info("*** Completed epoch {} ***", i);
         }
 
-
-        log.info ("Evaluate model....");
-        Evaluation eval = new Evaluation (outputNum);
-        while (testIter.hasNext ()) {
-            DataSet t = testIter.next ();
-            INDArray features = t.getFeatureMatrix ();
-            INDArray labels = t.getLabels ();
-            INDArray predicted = model.output (features, false);
-            eval.eval (labels, predicted);                          // Comparing the label predicted with the original label of the test data
+        log.info("Evaluate model....");
+        Evaluation eval = new Evaluation(outputNum);
+        while (testIter.hasNext()) {
+            DataSet next = testIter.next();
+            INDArray features = next.getFeatureMatrix();
+            INDArray Labels = next.getLabels();
+            INDArray prediction = model.output(features, false);
+            eval.eval(Labels, prediction);
         }
-
-
-        log.info (eval.stats ());
-        log.info ("**************** FINISH ********************");
+        log.info(eval.stats());
+        log.info("**************** FINISH ********************");
 
     }
 }
